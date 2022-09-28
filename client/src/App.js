3e55fbb1042d0fe7877e30b72ddc57af4b023234
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2022-09-28 23:27:44 +0530

    Login Page Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './pages/Login/Login'
 
 function App() {
   return (
-    <div className="main"></div>
+    <div className="main">
+       <Login />
+    </div>
   );
 }
 
