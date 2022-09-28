bd09c0b97d9aa4d60f78e9e68ce7f9f970f0e352
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2022-09-28 23:42:17 +0530

    Login Change

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Login=()=>{
     return(
         <div className="loginContainer">
-
+            <div className="loginCredentials">
+                
+            </div>
         </div>
     )
 }
