8410a23ad3b28e63a62d2ebb8d34795c051caf60
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2022-09-30 00:12:02 +0530

    Misc Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route,Switch} from 'react-router-dom'
 import Sign from './pages/SignUp/Sign'
 import Login from './pages/Login/Login'
+import Profile from './pages/Profile/Profile'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
          <Route path="/login" component={Login} />
          <Route path="/signup" component={Sign} />
+         <Route path="/profile" component={Profile} />
       </Switch>
     </BrowserRouter>
   );
