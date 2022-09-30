12975e1fa9b5c8eeb927771740e7340a8cf79b06
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2022-09-30 11:03:40 +0530

    Profile Page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 const Profile=()=>{
     return(
-        <div className="main">
+        <div className="profileMain">
             <div className="profileContainer">
+                <div className="topSection">
+                    <div className="profilePicSection">
+                       <div className="profilePic">
+                           <img src="docs/img/img1.jpg" />
+                       </div>
+                    </div>
+                    <div className="topData">
+                      <div className="topProfile">
+                          <div className="userName">
+                              <h2>incredibleindia</h2>
+                          </div>
+                          <div className="userButtons">
+                          </div>        
+                      </div>
+                    </div>
+                </div>    
                 <div className="imageSection">
                     <img src="docs/img/img1.jpg" />
                     <img src="docs/img/img1.jpg" />
