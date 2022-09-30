0f94e125f92eed3c069154363fd6cb4f9a891c09
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2022-09-30 23:51:43 +0530

    Profile Page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -15,9 +15,34 @@ const Profile=()=>{
                           <div className="userName">
                               <h2>incredibleindia</h2>
                           </div>
-                          <div className="userButtons">
+                          <div className="userButtonSection">
+                              <button className="userButton">
+                                 <p>Message</p>
+                              </button>
+                              <button className="userButton">
+                                 <p>Follow</p>    
+                              </button>
                           </div>        
                       </div>
+                      <div className="profileCount">
+                          <div className="countUnit">
+                              <p><span>800</span> posts</p>
+                          </div>
+                          <div className="countUnit">
+                              <p><span>32k</span> Followers</p>
+                          </div>
+                          <div className="countUnit">
+                              <p><span>8000</span> posts</p>
+                          </div>
+                      </div>
+
+                      <div className="profileName">
+                          <p>Incredible India</p>
+                          <div clasName="profileDesc">
+                            <p>{"Welcome to the official Instagram page of Incredible India | Tag us or use #IncredibleIndia to grant us permission to repost."}</p>
+                          </div>
+                      </div>
+                      
                     </div>
                 </div>    
                 <div className="imageSection">
